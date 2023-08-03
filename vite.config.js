2337fa5331e134c887216ce0340991d80a87ec58
EDUhappyday0416 <n1096804@ntub.edu.tw>
2023-08-03 21:45:11 +0800

    fix

--- vite.config.js
+++ vite.config.js
@@ -5,12 +5,9 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:"/my-repo/",
   build: {
     outDir: './dist'
-  },
-  scripts: {
-    build: "vite build",
-    preview: "vite preview"
   },
   plugins: [
     vue(),
