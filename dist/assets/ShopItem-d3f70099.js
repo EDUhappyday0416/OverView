d95c2e62cdb2e2491fba85fdf2ff6cd789515019
frances <n1096804@ntub.edu.tw>
2023-08-20 14:22:04 +0800

    deploy

--- dist/assets/ShopItem-d3f70099.js
+++
@@ -1,1 +0,0 @@
-import{m as e,k as o,o as s,c}from"./index-34887d54.js";const a={class:"shoppingCart"},m={__name:"ShopItem",setup(r){const t=e();return o(()=>t.data),(n,p)=>(s(),c("div",a))}};export{m as default};
