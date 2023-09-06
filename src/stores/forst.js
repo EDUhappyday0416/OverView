ca8ec4d96562b0c018d021b947f27f332d8c0da6
EDUhappyday0416 <Frances_Cheng@compal.com>
2023-09-06 14:04:00 +0800

    add

---
+++ src/stores/forst.js
@@ -0,0 +1,33 @@
+import { defineStore } from 'pinia'
+import { getForstInfo  ,getForstData} from '../api/forest'
+export const useForstData = defineStore("forstData", {
+    state: () => ({ 
+        forstInfo: [],
+        queryInfo:[]
+     }),
+    getters: {},
+    actions: {
+        async getForstInfoMethod(startDate,endDate) {
+            try {
+                const { data } = await getForstInfo(startDate,endDate);
+                console.log(data)
+                this.forstInfo = data;
+                return data
+                
+            } catch (error) {
+                return Promise.reject(error.message);
+            }
+        },
+        async getQueryForst(Region,heigh,pagesize) {
+            try {
+                const { data } = await getForstData(Region,heigh,pagesize);
+                this.queryInfo = data;
+                return data
+                
+            } catch (error) {
+                return Promise.reject(error.message);
+            }
+        }
+
+    }
+});
