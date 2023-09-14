0b9ebcae968704814d46a195c0b3131a46549454
EDUhappyday0416 <73332543+EDUhappyday0416@users.noreply.github.com>
2023-09-14 23:38:27 +0800

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -57,7 +57,8 @@ CORS_ALLOWED_ORIGINS = [
     "https://127.0.0.1:5173",
     "http://localhost:8000",
     "http://127.0.0.1:9000",
-    "http://localhost:5173"
+    "http://localhost:5173",
+    "http://172.104.86.216"
     # "http://172.104.86.216:8000"
 
 ]
