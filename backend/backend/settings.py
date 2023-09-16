f0fb950e82de92020e7a5eb67bed0a2b85ecab78
frances <n1096804@ntub.edu.tw>
2023-09-16 23:14:19 +0800

    Set CORS_ALLOWED_ORIGINS

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -52,12 +52,12 @@ MIDDLEWARE = [
     "django.middleware.common.CommonMiddleware",
 ]
 
-CORS_ORIGIN_ALLOW_ALL = True 
+CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 
 CORS_ALLOWED_ORIGINS = [
-    # "https://localhost:5173",  
-
+    # "https://localhost:5173",
+    "https://frances.sinami.com",
     "https://127.0.0.1:5173",
     "http://localhost:8000",
     "http://127.0.0.1:9000",
