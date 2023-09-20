45cb2b396b805f246339ab1ad22f5b83c19e60f6
frances <n1096804@ntub.edu.tw>
2023-09-20 22:57:52 +0800

    新增表單過濾

---
+++ src/components/MountainFormText.vue
@@ -0,0 +1,43 @@
+<template>
+  <template v-if="item.type == 'text'">
+    <!-- <v-text-field
+      :label="item.label"
+      @input="$emit('update:modelValue', $event.target.value)"
+      :value="modelValue"
+    ></v-text-field> -->
+    <v-text-field :label="item.label"></v-text-field>
+  </template>
+  <template v-if="item.type == 'checkbox'">
+    <v-checkbox :label="item.label" type="checkbox"></v-checkbox>
+  </template>
+  <template v-if="item.type == 'select'">
+    <v-select :label="item.label" :items="data"></v-select>
+  </template>
+</template>
+
+<script setup>
+import { defineProps } from 'vue'
+
+const props = defineProps({
+  item: {
+    type: Object
+  },
+  modelValue: String,
+  type: {
+    type: String
+  },
+  field: {
+    type: Boolean
+  },
+  data: {
+    type: Array
+  },
+  vee: {
+    type: [String, Array]
+  }
+})
+
+const emit = defineEmits(['update:modelValue'])
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/MountainForm.vue
+++ src/views/MountainForm.vue
@@ -1,125 +1,105 @@
 <template>
-    <div class="mountainForm">
-        <v-row no-gutters>
-        <v-col
-            class="pa-2"
-            v-for="n in 3"
-            :key="n"
-            cols="12"
-            sm="4"
-        >
-            <v-text-field
-                label="爬山地點"
-            ></v-text-field>
-        </v-col>
-        </v-row>
-        <!-- <v-row no-gutters class="d-flex justify-space-between mx-auto">
-            <v-col class="ma-2" cols="12" sm="12" md="3">
-                <v-text-field
-                    label="爬山地點"
-                ></v-text-field>
-            </v-col>
-            <v-col class="ma-2" cols="12" sm="12" md="3">
-                <v-text-field
-                    label="所在縣市"
-                ></v-text-field>
-            </v-col>
-            <v-col class="ma-2" cols="12" sm="12" md="3">
-                <v-text-field
-                    label="起登地點"
-                ></v-text-field>
-            </v-col>
-            <v-col class="ma-2" cols="12" sm="12" md="3">
-                <v-text-field
-                    label="日期"
-                ></v-text-field>
-            </v-col>
-            <v-col class="ma-2" cols="12" sm="12" md="3">
-                <v-text-field
-                    label="聯絡資訊"
-                ></v-text-field>
-            </v-col>
-            <v-col class="ma-2" cols="12" sm="12" md="3">
-                <v-text-field
-                    label="交通方式"
-                ></v-text-field>
-            </v-col>
-            <v-col class="ma-2" cols="12" sm="12" md="3">
-                <v-text-field
-                    label="集合地點"
-                ></v-text-field>
-            </v-col>
-            <v-col class="ma-2" cols="12" sm="12" md="3">
-                <v-text-field
-                    label="起登時間"
-                ></v-text-field>
-            </v-col>
-            <v-col class="ma-2" cols="12" sm="12" md="3">
-                <v-text-field
-                    label="能力要求"
-                ></v-text-field>
-            </v-col>
-            <v-col class="ma-2" cols="12" sm="12" md="3">
-                <v-text-field
-                    label="揪團類型"
-                ></v-text-field>
-            </v-col>
-        </v-row> -->
-    </div>
+  <div class="mountainForm">
+    <v-row no-gutters>
+      <v-col class="pa-2" v-for="(item, index) in formData" :key="index" cols="12" sm="4">
+        <MountainFormText :item="item" :type="item.type" :field="item.field" :data="items" />
+      </v-col>
+
+      <pre>values: {{ values }}</pre>
+      <pre>errors: {{ errors }}</pre>
+    </v-row>
+  </div>
 </template>
 <script setup>
-import { ref } from 'vue'
-import { useField, useForm } from 'vee-validate'
+import { ref, reactive } from 'vue'
+// import { useField, useForm } from 'vee-validate'
+import { useForm } from 'vee-validate'
+import * as yup from 'yup'
+import MountainFormText from '../components/MountainFormText.vue'
 
-const { handleSubmit, handleReset } = useForm({
-    validationSchema: {
-    name (value) {
-        if (value?.length >= 2) return true
+// const name = useField('name')
+// const phone = useField('phone')
+// const email = useField('email')
+// const select = useField('select')
+// const checkbox = useField('checkbox')
 
-        return 'Name needs to be at least 2 characters.'
-    },
-    phone (value) {
-        if (value?.length > 9 && /[0-9-]+/.test(value)) return true
-
-        return 'Phone number needs to be at least 9 digits.'
-    },
-    email (value) {
-        if (/^[a-z.-]+@[a-z.-]+\.[a-z]+$/i.test(value)) return true
-
-        return 'Must be a valid e-mail.'
-    },
-    select (value) {
-        if (value) return true
-
-        return 'Select an item.'
-    },
-    checkbox (value) {
-        if (value === '1') return true
-
-        return 'Must be checked.'
-    },
-    },
+const items = ref(['Item 1', 'Item 2', 'Item 3', 'Item 4'])
+const { values, errors, defineInputBinds } = useForm({
+  validationSchema: yup.object({
+    email: yup.string().email().required()
+  })
 })
-const name = useField('name')
-const phone = useField('phone')
-const email = useField('email')
-const select = useField('select')
-const checkbox = useField('checkbox')
 
-const items = ref([
-    'Item 1',
-    'Item 2',
-    'Item 3',
-    'Item 4',
+const email = defineInputBinds('email', {
+  validateOnInput: true
+})
+
+const formData = ref([
+  {
+    label: '爬山地點',
+    type: 'text',
+    field: true,
+    key: email
+  },
+  {
+    label: '所在縣市',
+    type: 'checkbox',
+    field: true,
+    key: ''
+  },
+  {
+    label: '起登地點',
+    type: 'select',
+    field: true,
+    key: ''
+  },
+  {
+    label: '日期',
+    type: 'text',
+    field: true,
+    key: ''
+  },
+  {
+    label: '聯絡資訊',
+    type: 'text',
+    field: true,
+    key: ''
+  },
+  {
+    label: '交通方式',
+    type: 'text',
+    field: true,
+    key: ''
+  },
+  {
+    label: '集合地點',
+    type: 'text',
+    field: true,
+    key: ''
+  },
+  {
+    label: '起登時間',
+    type: 'text',
+    field: true,
+    key: ''
+  },
+  {
+    label: '能力要求',
+    type: 'text',
+    field: true,
+    key: ''
+  },
+  {
+    label: '揪團類型',
+    type: 'text',
+    field: true,
+    key: ''
+  }
 ])
-
-const submit = handleSubmit(values => {
-    alert(JSON.stringify(values, null, 2))
-})
 </script>
 <style lang="scss" scoped>
 .mountainForm {
-    width: 100%;
-    margin: 0 auto;
+  width: 100%;
+  margin: 0 auto;
 }
 </style>
